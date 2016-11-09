e081650388b4e674e3462cfec8be7b45152cbd8a
Paweł Abramowicz <pawelabrams@gmail.com>
2016-11-09 02:01:25 +0100

    Finished changing algos logic. Added Caesar, ROT13 algorithms

--- js/main.js
+++ js/main.js
@@ -26,24 +26,81 @@ var app = {
     decryptionOptions: {},
     algos: {
         "caesar": {
+            startCode: 65,
+            mod: 26,
             encryption: function (plaintext) {
-                return $("#move").value;
+                const move = parseInt(app.encryptionOptions.move);
+                const start = this.startCode;
+                const mod = this.mod;
+                let cyphertext = "";
+                plaintext = plaintext.toUpperCase();
+
+                for (let i = 0; i < plaintext.length; i++) {
+                    let letter = plaintext.charCodeAt(i);
+                    if (letter >= start && letter < start + mod) {
+                        cyphertext += String.fromCharCode((letter - start + move) % mod + start);
+                    } else {
+                        cyphertext += String.fromCharCode(letter);
+                    }
+                }
+                return cyphertext;
             },
             decryption: function (cyphertext) {
+                const move = parseInt(app.decryptionOptions.move);
+                const start = this.startCode;
+                const mod = this.mod;
+                let plaintext = "";
+                cyphertext = cyphertext.toUpperCase();
 
+                for (let i = 0; i < cyphertext.length; i++) {
+                    let letter = cyphertext.charCodeAt(i);
+                    if (letter >= start && letter < start + mod) {
+                        plaintext += String.fromCharCode((letter - start - move + mod) % mod + start);
+                    } else {
+                        plaintext += String.fromCharCode(letter);
+                    }
+                }
+                return plaintext;
             },
-        }
+        },
+        "rot13": {
+            startCode: 65,
+            mod: 26,
+            move: 13,
+            encryption: function (plaintext) {
+                const move = this.move;
+                const start = this.startCode;
+                const mod = this.mod;
+                let cyphertext = "";
+                plaintext = plaintext.toUpperCase();
+
+                for (let i = 0; i < plaintext.length; i++) {
+                    let letter = plaintext.charCodeAt(i);
+                    if (letter >= start && letter < start + mod) {
+                        cyphertext += String.fromCharCode((letter - start + move) % mod + start);
+                    } else {
+                        cyphertext += String.fromCharCode(letter);
+                    }
+                }
+                return cyphertext;
+            },
+            decryption: function (cyphertext) {
+                return this.encryption(cyphertext);
+            },
+        },
     },
     algosOptions: {
         "caesar": {
             "move": "number"
+        },
+        "rot13": {
         },
     }
 };
 
 (function() {
     // Change enc/dec options
-    function changeOptions(element, algo) {
+    function changeOptions(element, algo, saveField) {
         element.innerHTML = '';
         for (i in app.algosOptions[algo]) {
             let toAdd = undefined;
@@ -51,8 +108,11 @@ var app = {
                 case "number":
                     toAdd = document.createElement("input");
                     toAdd.type = "number";
-                    toAdd.id = i;
+                    toAdd.name = i;
             }
+            on(toAdd, "change", function() {
+                app[saveField][i] = this.value;
+            });
             element.appendChild(toAdd);
         }
     }
@@ -61,11 +121,11 @@ var app = {
     on(window, 'load', function () {
         on($("#encryptionAlgo"), 'change', function() {
             app.encryptionAlgo = $("#encryptionAlgo").value;
-            changeOptions($("#encryptionOptions"), app.encryptionAlgo);
+            changeOptions($("#encryptionOptions"), app.encryptionAlgo, "encryptionOptions");
         });
         on($("#decryptionAlgo"), 'change', function() {
             app.decryptionAlgo = $("#decryptionAlgo").value;
-            changeOptions($("#decryptionOptions"), app.decryptionAlgo);
+            changeOptions($("#decryptionOptions"), app.decryptionAlgo, "decryptionOptions");
         });
         on($("#encrypt"), 'click', function() {
             $("#communication").innerHTML = app.algos[app.encryptionAlgo].encryption($("#plaintext").value);
