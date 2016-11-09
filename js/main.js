5a7aa26c5b45633f6d7a918387f6e48bac0a02ba
Paweł Abramowicz <pawelabrams@gmail.com>
2016-11-09 02:35:55 +0100

    Vigenere algorithm added

--- js/main.js
+++ js/main.js
@@ -88,12 +88,57 @@ var app = {
                 return this.encryption(cyphertext);
             },
         },
+        "vigenere": {
+            startCode: 65,
+            mod: 26,
+            encryption: function (plaintext) {
+                const key = app.encryptionOptions.key.toUpperCase();
+                const keylen = key.length;
+                const start = this.startCode;
+                const mod = this.mod;
+                let cyphertext = "";
+                plaintext = plaintext.toUpperCase();
+
+                for (let i = 0; i < plaintext.length; i++) {
+                    let letter = plaintext.charCodeAt(i);
+                    let move = key.charCodeAt(i % keylen) - start;
+                    if (letter >= start && letter < start + mod) {
+                        cyphertext += String.fromCharCode((letter - start + move) % mod + start);
+                    } else {
+                        cyphertext += String.fromCharCode(letter);
+                    }
+                }
+                return cyphertext;
+            },
+            decryption: function (cyphertext) {
+                const key = app.encryptionOptions.key.toUpperCase();
+                const keylen = key.length;
+                const start = this.startCode;
+                const mod = this.mod;
+                let plaintext = "";
+                cyphertext = cyphertext.toUpperCase();
+
+                for (let i = 0; i < cyphertext.length; i++) {
+                    let letter = cyphertext.charCodeAt(i);
+                    let move = key.charCodeAt(i % keylen) - start;
+                    if (letter >= start && letter < start + mod) {
+                        plaintext += String.fromCharCode((letter - start - move + mod) % mod + start);
+                    } else {
+                        plaintext += String.fromCharCode(letter);
+                    }
+                }
+                return plaintext;
+            },
+        },
     },
     algosOptions: {
         "caesar": {
             "move": "number"
         },
         "rot13": {
+        },
+        "vigenere": {
+            "key": "text"
         },
     }
 };
@@ -106,8 +151,9 @@ var app = {
             let toAdd = undefined;
             switch(app.algosOptions[algo][i]) {
                 case "number":
+                case "text":
                     toAdd = document.createElement("input");
-                    toAdd.type = "number";
+                    toAdd.type = app.algosOptions[algo][i];
                     toAdd.name = i;
             }
             on(toAdd, "change", function() {
