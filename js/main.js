8fb30d99ae10df170b33e1f20ea37740e95339b4
Paweł Abramowicz <pawelabrams@gmail.com>
2016-11-15 18:23:28 +0100

    Communication added

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ function on(context, event, fn) {
 }
 
 var app = {
+    context: {},
     encryptionAlgo: null,
     encryptionOptions: {},
     decryptionAlgo: null,
@@ -163,8 +164,34 @@ var app = {
         }
     }
 
+    // Bind app.context: .received and .connection and make hangout visible
+    function openHangout(conn) {
+        conn.on('open', function() {
+            // Receive messages
+            app.context.received = [];
+            conn.on('data', function(data) {
+                console.log('Received '+data);
+                app.context.received.push(data);
+                window.dispatchEvent(new CustomEvent('dataReceive', {'detail': data}));
+            });
+            // Send messages
+            app.context.connection = conn;
+            // Make hangout visible
+            $(".connect").style.display = 'none';
+            $(".hangout").style.display = 'block';
+        });
+    }
+
     // Add those functions
     on(window, 'load', function () {
+        // peerjs
+        window.peer = new Peer(undefined, { host: 'abramowicz.org', port: 9517, path: '/qomm'});
+        peer.on('open', function(id) { $("#yourId").value = id; });
+        peer.on('connection', openHangout);
+        on($("#connectButton"), 'click', function() {
+            openHangout(peer.connect($("#connectId").value));
+        });
+
         on($("#encryptionAlgo"), 'change', function() {
             app.encryptionAlgo = $("#encryptionAlgo").value;
             changeOptions($("#encryptionOptions"), app.encryptionAlgo, "encryptionOptions");
@@ -174,10 +201,16 @@ var app = {
             changeOptions($("#decryptionOptions"), app.decryptionAlgo, "decryptionOptions");
         });
         on($("#encrypt"), 'click', function() {
-            $("#communication").innerHTML = app.algos[app.encryptionAlgo].encryption($("#plaintext").value);
+            let cyphertext = app.algos[app.encryptionAlgo].encryption($("#plaintext").value);
+            $("#communication").innerHTML = cyphertext;
+            app.context.connection.send(cyphertext);
+        });
+        on(window, 'dataReceive', function (e) {
+            console.log(e);
+            $("#decoded").innerHTML = app.algos[app.decryptionAlgo].decryption(e.detail);
         });
         on($("#decrypt"), 'click', function() {
-            $("#decoded").innerHTML = app.algos[app.decryptionAlgo].decryption($("#communication").innerHTML);
+            $("#decoded").innerHTML = app.algos[app.decryptionAlgo].decryption(app.context.received.slice(-1)[0]);
         });
     });
 })();
